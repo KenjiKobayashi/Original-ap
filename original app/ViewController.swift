e48cbf934b812868cae30afdc34610e8d7e0a774
Kenji Kobayashi <yugokobahabit@gmail.com>
2017-09-06 23:43:58 +0900

    よくわからん

--- original app/ViewController.swift
+++ original app/ViewController.swift
@@ -9,11 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count = 0
+    var timer = Timer()
+    @IBOutlet var TwitterLabel: UILabel!
+    let saveData: UserDefaults = UserDefaults.standard
+    
+    @IBOutlet var totallabel: UILabel!
+  
+    
+    @IBAction func schemebtn() {
+        let url = URL(string: "com.atebits.Tweetie2://")!
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.openURL(url)
+        }
+        if !timer.isValid {
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(self.up),
+            userInfo: nil,
+            repeats: true )
+        }
+    }
+    func up() {
+        count = count + 1
+        TwitterLabel.text = String(count)
+    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        if timer.isValid {
+//            timer.invalidate()
+//            saveData.set(TwitterLabel.text, forKey: "time")
+//            saveData.synchronize()
+//        }
+//    }
+//    
+
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(self.stopTimer(notification:)), name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
         // Do any additional setup after loading the view, typically from a nib.
+        totallabel.text = saveData.object(forKey: "time") as! String?
     }
+    
+    func stopTimer(notification: Notification) {
+        print(timer.isValid)
+        if timer.isValid {
+            timer.invalidate()
+            saveData.set(TwitterLabel.text, forKey: "time")
+            saveData.synchronize()
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
